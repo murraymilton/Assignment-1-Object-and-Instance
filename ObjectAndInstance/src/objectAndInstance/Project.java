83437179ba5fc7dabbeeb85028bb29f8cb3cde72
murraymilton <murray.l.milton@outlook.com>
2023-03-16 14:25:06 +0100

    added the find maximum method for the instance object created for the Weight class object

--- ObjectAndInstance/src/objectAndInstance/Project.java
+++ ObjectAndInstance/src/objectAndInstance/Project.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 public class Project {
 
-
     /**
      * private class method for returning the minimum of the weight objects
      * @param weight1
@@ -12,6 +11,9 @@ public class Project {
      * @param weight3
      * @return
      */
+
+    //Invoking the lessThan method for
+
     private Weight findMinimum(Weight weight1, Weight weight2, Weight weight3){
         if(weight1.lessThan(weight2) && weight1.lessThan(weight3)){
             return weight1;
@@ -22,4 +24,18 @@ public class Project {
         return weight3;
     }
 //    private Weight findMaximum
+
+    private Weight findMaximum(Weight weight1, Weight weight2, Weight weight3){
+        if(weight2.lessThan(weight1) && weight3.lessThan(weight1)){
+            return weight1;
+        } else if (weight1.lessThan(weight2) && weight3.lessThan(weight2)) {
+            return weight2;
+        } else if (weight1.lessThan(weight3) && weight2.lessThan(weight3)) {
+
+        }
+        return weight3;
+    }
+    private static void main(String[]args){
+        
+    }
 }
